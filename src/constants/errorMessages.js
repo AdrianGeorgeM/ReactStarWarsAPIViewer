5df7b8fb445d295701640e615bd1b5033500fbcc
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-09-22 10:25:23 +0100

    Update Constant for errorMessage

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,3 +1,3 @@
 export const INVALID_QUERY_MESSAGE =
-	'Please enter a valid query: starships, films, or vehicles';
+	'Please enter a valid query:films, starships or vehicles';
 export const FETCH_ERROR_MESSAGE = "The Force isn't with us today.";
